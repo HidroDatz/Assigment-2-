315269551046ee211a7a4dcbdef460961c2cb397
HidroDatz <98503129+HidroDatz@users.noreply.github.com>
2022-07-16 14:30:19 +0700

    Delete SQLQuery1.sql

--- Assigment 2/Assigment 2/SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-SELECT s.First_Name + ' ' + s.Last_Name  AS [Name], c.CouID AS Course FROM 
-Students s INNER JOIN Grade G ON s.StudentID = g.StudentID
-INNER JOIN ASSESSMENT_SYSTEM sa ON sa.AssID = G.AssID
-INNER JOIN Courses c ON c.CouID = sa.CouID
-WHERE (G.score = 0) OR G.score < 4 
-GROUP BY s.First_Name + s.Last_Name,c.CouID
-HAVING (SUM(G.score * sa.Weigh) < 5) OR MIN(G.score) = 0
